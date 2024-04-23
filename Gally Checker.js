c7126128f7bae431d3668b11b591fdd44451f0b4
Oliver <Olivermoser.jm@gmail.com>
2024-04-23 12:23:13 -0600

    does this steal your collections

--- Gally Checker.js
+++ Gally Checker.js
@@ -1,5 +1,7 @@
+const apiKey = "d95853023d6143c89b5dd62c4c0ebdf9";
+
 const myHeaders = new Headers();
-myHeaders.append("X-API-KEY", "d95853023d6143c89b5dd62c4c0ebdf9");
+myHeaders.append("X-API-KEY", apiKey);
 
 const requestOptions = {
     method: "GET",
@@ -8,6 +10,13 @@ const requestOptions = {
 };
 
 fetch("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/Oliver%20the%20crow%233439/", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+    .then(response => response.json())
+    .then(result => {
+        const player = result.Response[0];
+        const membershipType = player.membershipType;
+        const membershipId = player.membershipId
+        console.log("Membership Type:", membershipType);
+
+        const url = `https://bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800`;
+    })
+    .catch(error => console.error(error));
