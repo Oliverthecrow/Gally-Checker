139825d0a402fa9b7eeb0276fe5ba598793928a8
Oliver <Olivermoser.jm@gmail.com>
2024-04-23 12:44:55 -0600

    lets see if this works

--- Gally Checker.js
+++ Gally Checker.js
@@ -1,4 +1,9 @@
 let apiKey = "d95853023d6143c89b5dd62c4c0ebdf9";
+let username = '';
+let membershipType = '';
+let membershipId = '';
+let SearchUrl = "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${membershipType}/${username}/";
+let CollectionsUrl = `https://bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800`;
 
 let myHeaders = new Headers();
 myHeaders.append("X-API-KEY", apiKey);
@@ -13,19 +18,20 @@ fetch("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/Oliver%20
     .then(response => response.json())
     .then(result => {
         let player = result.Response[0];
-        let membershipType = player.membershipType;
-        let membershipId = player.membershipId;
+        membershipType = player.membershipType;
+        membershipId = player.membershipId;
         console.log("Membership Type:", membershipType);
         console.log("MembershipID", membershipId);
-
-        let url = `https://bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800`;
     })
+fetch(CollectionsUrl, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        let GallyId = '1363886209';
+        let itemComponent = data.Response.itemComponents.collectibles.data;
 
-let GallyId = '1363886209';
-let itemComponent = data.Response.itemComponents.collectibles.data;
-
-// Check if the player has the Gjallarhorn
-if (itemComponent[GallyId]) {
-    console.log('Player has Gjallarhorn');
-}
-else {console.log('Player does not have Gjallarhorn')}
+        // Check if the player has the Gjallarhorn
+        if (itemComponent[GallyId]) {
+            console.log('Player has Gjallarhorn');
+        }
+        else { console.log('Player does not have Gjallarhorn') }
+    })
