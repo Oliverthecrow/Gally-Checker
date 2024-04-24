796a6330dc3f249ec9f2704aa74edbf47a502918
Oliverthecrow <Olivermoser.jm@gmail.com>
2024-04-24 14:03:24 -0600

    asdfasdf

--- Gally Checker.js
+++ Gally Checker.js
@@ -1,25 +1,9 @@
 let apiKey = 'd95853023d6143c89b5dd62c4c0ebdf9';
-let membershipId = '';
-let membershipType = '';
-let SearchUrl = 'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${membershipType}/${username}/';
-let ExoticsOwned = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-
-let myHeaders = new Headers();
-myHeaders.append('X-API-Key', apiKey);
-myHeaders.append('Authorization', '46542');
-
-let requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
 
 async function getUserFromName(name) {
     let headers = new Headers();
     headers.append('X-API-KEY', apiKey);
     headers.append('Authorization', '46542');
-    console.log('Membership type' + membershipType);
-    console.log('Membership Id' + membershipId);
     let opts = {
         method: 'POST',
         body: JSON.stringify({
@@ -29,37 +13,52 @@ async function getUserFromName(name) {
         headers: headers,
         redirect: 'follow',
     };
-    user = await fetch(`https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayerByBungieName/-1`, opts).then((response) => response.json().then());
+
+    user = await fetch(
+        `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayerByBungieName/-1`,
+        opts
+    ).then((response) => response.json().then());
     return user.Response;
 }
 
 async function ExoticChecker(name) {
-    let user = getUserFromName(name);
+    let myHeaders = new Headers();
+    myHeaders.append('X-API-KEY', apiKey);
+    myHeaders.append('Authorization', '46542');
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+    };
+    let user = await getUserFromName(name);
     if (!user[0]) {
         return `user '${name}' does not exist.`;
     }
     let firstMembership = user[0];
-    membershipType = firstMembership.membershipType;
-    membershipId = firstMembership.membershipId;
-    fetch('https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800', requestOptions)
-        .then((response) => response.json().then((result) => {
+    let membershipType = firstMembership.membershipType;
+    let membershipId = firstMembership.membershipId;
+    return fetch(
+        `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800`,
+        requestOptions
+    ).then((response) =>
+        response.json().then((result) => {
             // every item that the player owns
-            let collectibles = result.Response.profileCollectibles.data.collectibles;
-            /* GallyId '4027219968' IziId = '24541428' LuminaId = '2924632392' WitherhoardID = '1250332035' TractorId = '2094776121'
-            Verity's Brow = '846189250' Cenotaph = '2940602493'
+            /* GallyId '4027219968' IziId = '3211806999' LuminaId = '3512014804' WithoardID = '2522817335' TractorId = '3580904581'
+            Verity's Brow = '2897117448' Cenotaph = '2374129871'
             */
-            let Exotics = ['4027219968', '24541428', '2924632392', '1250332035', '2094776121', '846189250', '2940602493'];
+            let Exotics = ['4027219968', '3211806999', '3512014804', '2522817335', '3580904581', '2897117448', '2374129871'];
 
             for (let i = 0; i < Exotics.length; i++) {
-                if (collectibles[Exotics[i]]) {
+                if (collectibles[Exotics[i]].state === 0) {
                     console.log('${name} has ' + Exotics[i]);
-                    ExoticsOwned.splice(i, 1, true);
                 }
                 else { console.log('${name} does not have ' + Exotics[i]); }
             };
-        }));
+        })
+    );
 }
-function StrandDetection() {
-
-}
-ExoticChecker('Oliver the crow#3439')
+ExoticChecker('Oliver the crow').then(console.log);
+ExoticChecker('Oliver the crow#3439').then(console.log);
+ExoticChecker('Unstable Light 0:01#9056').then(console.log);
+ExoticChecker('very real username').then(console.log);
+ExoticChecker('mixed wrestling#8577').then(console.log);
