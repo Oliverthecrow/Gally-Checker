8c5c243dcdbe6ec34f4eb4ed47963da84804ba95
Oliver <Olivermoser.jm@gmail.com>
2024-04-24 11:12:25 -0600

    yo

--- Gally Checker.js
+++ Gally Checker.js
@@ -1,9 +1,8 @@
 let apiKey = 'd95853023d6143c89b5dd62c4c0ebdf9';
-let username = '';
 let membershipId = '';
 let membershipType = '';
 let SearchUrl = 'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${membershipType}/${username}/';
-let ExoticsOwned = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+let ExoticsOwned = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
 
 let myHeaders = new Headers();
 myHeaders.append('X-API-Key', apiKey);
@@ -14,34 +13,52 @@ let requestOptions = {
     headers: myHeaders,
     redirect: 'follow'
 };
-function findPlayer(Url) {
-    fetch(Url, requestOptions)
-        .then(response => response.json().then(result => {
-            let player = result.Response[0];
-            membershipType = player.membershipType;
-            membershipId = player.membershipId;
-            console.log('Membership Type:', membershipType);
-            console.log('MembershipID', membershipId);
-        }))
+
+function getUserFromName(name) {
+    let headers = new Headers();
+    headers.append("X-API-KEY", apiKey);
+    headers.append("Authorization", "46542");
+    let opts = {
+        method: "POST",
+        body: JSON.stringify({
+            displayName: name.split("#")[0],
+            displayNameCode: name.split("#")[1],
+        }),
+        headers: headers,
+        redirect: "follow",
+    };
+    user = fetch(`https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayerByBungieName/-1`, opts).then((response) => response.json().then());
+    return user.Response;
 }
-function ExoticChecker(type, name) {
-    fetch('https://www.bungie.net/Platform/Destiny2/' + type.toString() + '/Profile/' + name.toString() + '/?components=800', requestOptions)
+
+function ExoticChecker(name) {
+    let user = getUserFromName(name);
+    if (!user[0]) {
+        return `user "${name}" does not exist.`;
+    }
+    let firstMembership = user[0];
+    membershipType = firstMembership.membershipType;
+    membershipId = firstMembership.membershipId;
+    fetch('https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=800', requestOptions)
         .then((response) => response.json().then((result) => {
             // every item that the player owns
             let collectibles = result.Response.profileCollectibles.data.collectibles;
-            /* GallyId '4027219968' IziId = '24541428' LuminaId = '2924632392' WithoardID = '1250332035' TractorId = '2094776121'
+            /* GallyId '4027219968' IziId = '24541428' LuminaId = '2924632392' WitherhoardID = '1250332035' TractorId = '2094776121'
             Verity's Brow = '846189250' Cenotaph = '2940602493'
             */
             let Exotics = ['4027219968', '24541428', '2924632392', '1250332035', '2094776121', '846189250', '2940602493'];
 
             for (let i = 0; i < Exotics.length; i++) {
                 if (collectibles[Exotics[i]]) {
-                    console.log('Player has ' + Exotics[i]);
+                    console.log('${name} has ' + Exotics[i]);
                     ExoticsOwned.splice(i, 1, true);
                 }
-                else { console.log('Player does not have ' + Exotics[i]); }
+                else { console.log('${name} does not have ' + Exotics[i]); }
             };
         }));
 }
-findPlayer('https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/Oliver%20the%20crow%233439/');
-ExoticChecker(membershipType, membershipId);
+function StrandDetection() {
+
+}
+findPlayer('Oliver the crow#3439').then(console.log);
+ExoticChecker();
